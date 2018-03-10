bdb430d3e2db32b7fe082d5748ee226e7f94966d
Geoffroy Van Cutsem <geoffroy.vancutsem@intel.com>
2018-03-10 22:40:56 +0100

    Experiments with the following modifications: * Use a video file as a source instead of IP camera * Detect Zombie objects * Run both detection algorithms in parallel
    Signed-off-by :Geoffroy Van Cutsem <geoffroy.vancutsem@intel.com>

--- webcam.py
+++ webcam.py
@@ -5,12 +5,15 @@ import time
 import redis
 
 ip_camera=os.environ['IP_CAMERA']
-cap = cv2.VideoCapture('rtsp://' + ip_camera + ':554/11')
+#cap = cv2.VideoCapture('rtsp://' + ip_camera + ':554/11')
+cap = cv2.VideoCapture('/streamapp/video/ed-720p.mp4')
 r = redis.StrictRedis(host='redis', port=6379, db=0)
 n_frames = 0
 start = time.time()
 while True:
     _, f = cap.read()
+    if (type(f) == type(None)):
+        cap = cv2.VideoCapture('/streamapp/video/ed-720p.mp4')
     n_frames += 1
     r.set('img', pickle.dumps(f))
     if n_frames % 10 == 0:
